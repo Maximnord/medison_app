9287e62b54b2933b0de272130e9ab9d0e742f517
Maxim Soskind <maximnord@gmail.com>
2023-06-04 00:52:39 +0300

    css and logo changes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    // public const HOME = '/dashboard';
+    public const HOME = '/countries';
 
     /**
      * The controller namespace for the application.
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        {{-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> --}}
+                        <img src="{{ asset('images/logo.jpg') }}" alt="Logo" class="w-40 h-15 fill-current text-gray-500">
                     </a>
                 </div>
 
